efebe9693fd21033aa6a4c6152f0e6f64bca0c6f
SteveCarpio <stv.madrid@gmail.com>
2026-03-26 19:37:39 +0100

    ok

--- main_app.py
+++ main_app.py
@@ -18,21 +18,52 @@ st.set_page_config(
 # 2. ESTILOS CSS PERSONALIZADOS (UI/UX)
 st.markdown("""
     <style>
-    /* Estilo para las 'Cards' de las aplicaciones */
-    .app-card {
-        background-color: #f8f9fa;
-        border-left: 5px solid #1c2e4a;
-        padding: 1.5rem;
+    /* Estilo para el Titulo */
+    .titulo-tda { 
+        color: #555; 
+        font-size: 0.7rem; 
+    }
+            
+    /* Estilo para Banner noticias */       
+    .banner {
+        background-color: #f0f2f6;  /* #FFF8F2; #FFEFDF;  #FFC099   #f8f9fa;*/
+        border-left: 0px;
+        padding: 0.5rem;
         border-radius: 0.5rem;
         margin-bottom: 1rem;
         transition: transform 0.2s;
         box-shadow: 2px 2px 10px rgba(0,0,0,0.05);
     }
-    .app-card:hover {
-        transform: translateY(-5px);
-        box-shadow: 2px 5px 15px rgba(0,0,0,0.1);
+    .banner-titulo { 
+        color: #FF6200; 
+        font-weight: bold; 
+        font-size: 0.7rem; 
+       /* text-transform: uppercase; */
     }
-    .fin-header {
+       
+    /* Estilo para las Tarjetas de aplicaciones */         
+    .app-card {
+        background-color: #FFF8F2;
+        border-left: 4px solid #1c2e4a;
+        padding: 0.8rem 1rem; /* Más compacto */
+        border-radius: 0.4rem;
+        margin-bottom: 0.8rem;
+        transition: transform 0.2s;
+        box-shadow: 1px 1px 6px rgba(0,0,0,0.05);
+    }
+  
+    .app-card:hover {
+        transform: translateX(5px); /* Movimiento lateral en lugar de hacia arriba */
+        box-shadow: 2px 2px 10px rgba(0,0,0,0.1);
+    }
+    .app-card h4 {
+        margin: 0; /* Elimina espacio extra del título */
+        color: #1c2e4a;
+        font-size: 1rem;
+    }
+
+
+    .app-card-titulo {
         color: #1c2e4a;
         font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
         font-weight: 700;
@@ -40,20 +71,27 @@ st.markdown("""
         padding-bottom: 10px;
         margin-top: 20px;
     }
+   
     .small-text-fin {
-        font-size: 0.85rem;
-        color: #555;
-        line-height: 1.4;
+        font-size: 0.75rem; /* Texto un poco más pequeño */
+        color: #666;
+        line-height: 1.2;
+        margin-top: 5px;
     }
-    .tag-ia { color: #FF6200; font-weight: bold; font-size: 0.7rem; text-transform: uppercase; }
+
+         
+    .tag-ia { 
+        color: #FF6200; 
+        font-weight: bold; 
+        font-size: 0.65rem; 
+        text-transform: uppercase; 
+        margin: 2px 0;
+    }
     
-    .tag-titulo-tda { color: #555; font-size: 0.7rem; }
-    
-    .tag-titulo-user-ok { color: #668555; font-size: 0.7rem; }
-    .tag-titulo-user-ko { color: #FF6200; font-size: 0.7rem; }
+
     /* Ajustes en el Sidebar */
     [data-testid="stSidebar"] {
-        background-color: #f0f2f6;
+        background-color: #f0f2f6; /* #f0f2f6; #FFF8F2; */
     }
     </style>
     """, unsafe_allow_html=True)
@@ -94,17 +132,12 @@ def get_image_base64(path):
 
 def mostrar_inicio():
     # HEADER CON LOGO
-    COL_ANCHO = 0.5
+    COL_ANCHO = 0.1
 
     col_t1, col_t2, col_t3 = st.columns([3, COL_ANCHO ,1])
     with col_t1:
-        st.caption("""
-                   -----------------------------------------------------------------
-
-                   📢 [Ticker Noticias / Indicadores] .............23234.......56456456.......rfg_34535........................
-
-                   -----------------------------------------------------------------
-                   """)
+        st.markdown('<div class="banner"> <p class="banner-titulo">📢 [ 2026-03-26 Noticias xxxxx x x x  ] </p><p class="small-text-fin">BMV 5% - Multiva 0.5%.</p></div>', unsafe_allow_html=True)
+    
         
         #st.markdown(f'<p class="tag-titulo-tda">⬅️ Seleccione una herramienta en el panel izquierdo para comenzar.</p>', unsafe_allow_html=True)
         
@@ -115,66 +148,48 @@ def mostrar_inicio():
             
         except: pass
 
-    #st.markdown("Bienvenido al portal analítico. Seleccione una herramienta en el panel izquierdo para comenzar.")
-    #st.markdown('<p class="tag-titulo-tda">Bienvenido al portal analítico. Seleccione una herramienta en el panel izquierdo para comenzar.</p>', unsafe_allow_html=True)
+    st.markdown('<h4><p class="tag-ia">Bienvenido al portal TDA - [ Banco Multiva ] </p></h4>', unsafe_allow_html=True)
 
     # --- 🏛️ Fideicomisos y Emisiones ---
-    st.markdown('<h3 class="fin-header">🏛️ Fideicomisos y Emisiones</h3>', unsafe_allow_html=True)
-    c1, c2, c3 = st.columns(3)
+    st.markdown('<h3 class="app-card-titulo">🏛️ Fideicomisos y Emisiones</h3>', unsafe_allow_html=True)
+    c1, c2= st.columns(2)
     with c1:
-        st.markdown('<div class="app-card"><h4>Emisores</h4><p class="tag-ia">Shared Folder</p><p class="small-text-fin">Datos de Documentos, etc, etc... etc.....</p></div>', unsafe_allow_html=True)
-        pass
-    with c2:
-        #st.markdown('<div class="app-card"><h4>Transcripción</h4><p class="tag-ia">Audio Intelligence</p><p class="small-text-fin">Conversión de voz a texto y generación automática de minutas.</p></div>', unsafe_allow_html=True)
-        pass
-    with c3:
-        #st.markdown('<div class="app-card"><h4>ChatTDA</h4><p class="tag-ia">Assistant</p><p class="small-text-fin">Interfaz conversacional inteligente con soporte para documentos internos.</p></div>', unsafe_allow_html=True)
-        pass
+        st.markdown('<div class="app-card"><p class="tag-ia">Emisores</p> <p class="small-text-fin">Datos de Documentos, etc, etc... etc.....</p></div>', unsafe_allow_html=True)
 
     # --- 🚨 Eventos Relevantes ---
-    st.markdown('<h3 class="fin-header">🚨 Eventos Relevantes</h3>', unsafe_allow_html=True)
-    cd1, cd2, cd3 = st.columns(3)
+    st.markdown('<h3 class="app-card-titulo">🚨 Eventos Relevantes</h3>', unsafe_allow_html=True)
+    cd1, cd2 = st.columns(2)
     with cd1:
-        st.markdown('<div class="app-card"><h4>xxxxx</h4><p class="tag-ia">xxxxx</p><p class="small-text-fin">xxxxxxxxxxxx.</p></div>', unsafe_allow_html=True)
-    with cd2:
-        #st.markdown('<div class="app-card"><h4>Flujos Bloomberg</h4> <p class="tag-ia">Treasury Operations</p>  <p class="small-text-fin">Procesamiento y conversión de flujos de inversión para la integración directa con terminales Bloomberg.</p></div>', unsafe_allow_html=True)
-        pass
-    with cd3:
-        pass
-        #st.markdown('<div class="app-card"><h4>Entidades Financieras</h4> <p class="tag-ia">Official Historical Registry</p> <p class="small-text-fin">Visualización y seguimiento histórico de las Entidades Financieras registradas en el Banco de España. </p></div>', unsafe_allow_html=True)
-
+        st.markdown('<div class="app-card"> <p class="tag-ia">Listado Histórico</p><p class="small-text-fin">xxxxxxxxxxxx.</p></div>', unsafe_allow_html=True)
+    
     # --- 📑 Reportes de Emisores ---
-    st.markdown('<h3 class="fin-header">📑 Reportes de Emisores</h3>', unsafe_allow_html=True)
-    cw1, cw2, cw3 = st.columns(3)
+    st.markdown('<h3 class="app-card-titulo">📑 Reportes de Emisores</h3>', unsafe_allow_html=True)
+    cw1, cw2 = st.columns(2)
     with cw1:
-        st.markdown('<div class="app-card"><h4>xxxxx</h4><p class="tag-ia">xxxxx</p><p class="small-text-fin">xxxxxxxxxxxx.</p></div>', unsafe_allow_html=True)                                                                                                                                         
-    with cw2:
-        #st.markdown('<div class="app-card"><h4>Estados Financieros</h4> <p class="tag-ia">Data Extraction</p>  <p class="small-text-fin">Extracción bajo demanda de reportes financieros oficiales desde el repositorio de la CNBV.</p></div>', unsafe_allow_html=True)
-        pass    
+        st.markdown('<div class="app-card"> <p class="tag-ia">Trimestrales</p><p class="small-text-fin">xxxxxxxxxxxx.</p></div>', unsafe_allow_html=True)                                                                                                                                         
+       
     # --- 📅 Calendario Operativo ---
-    st.markdown('<h3 class="fin-header">📅 Calendario Operativo</h3>', unsafe_allow_html=True)
-    cr1, cr2, cr3 = st.columns(3)
+    st.markdown('<h3 class="app-card-titulo">📅 Calendario Operativo</h3>', unsafe_allow_html=True)
+    cr1, cr2= st.columns(2)
     with cr1:
-        st.markdown('<div class="app-card"><h4>Publicaciones</h4> <p class="tag-ia">xxxxxx</p> <p class="small-text-fin">xxxxxxxxx. </p></div>', unsafe_allow_html=True)
+        st.markdown('<div class="app-card"> <p class="tag-ia">Publicaciones</p> <p class="small-text-fin">xxxxxxxxx. </p></div>', unsafe_allow_html=True)
     with cr2:
-        st.markdown('<div class="app-card"><h4>Festivos</h4> <p class="tag-ia">xxxxxxxx</p>  <p class="small-text-fin">xxxxxxxxxxxxxxx.</p></div>', unsafe_allow_html=True) 
-    with cr3:
-        pass
-        #st.markdown('<div class="app-card"><h4>Analizador Financiero</h4> <p class="tag-ia">Banxico & Yahoo Finance API</p>  <p class="small-text-fin">Extracción y visualización de indicadores económicos y bursátiles históricos para análisis corporativo.</p></div>', unsafe_allow_html=True)
-
-
+        st.markdown('<div class="app-card"> <p class="tag-ia">Festivos</p>  <p class="small-text-fin">xxxxxxxxxxxxxxx.</p></div>', unsafe_allow_html=True) 
+    
     # --- ✅ Tareas Programadas ---
-    st.markdown('<h3 class="fin-header">✅ Tareas Programadas</h3>', unsafe_allow_html=True)
-    cw1, cw2, cw3 = st.columns(3)
+    st.markdown('<h3 class="app-card-titulo">✅ Tareas Programadas</h3>', unsafe_allow_html=True)
+    cw1, cw2 = st.columns(2)
     with cw1:
-        st.markdown('<div class="app-card"><h4>xxxxx</h4><p class="tag-ia">xxxxx</p><p class="small-text-fin">xxxxxxxxxxxx.</p></div>', unsafe_allow_html=True)                                                                                                                                         
+        st.markdown('<div class="app-card"> <p class="tag-ia">Semanales</p><p class="small-text-fin">xxxxxxxxxxxx.</p></div>', unsafe_allow_html=True)                                                                                                                                         
+    with cw2:
+        st.markdown('<div class="app-card"> <p class="tag-ia">Mensuales</p><p class="small-text-fin">xxxxxxxxxxxx.</p></div>', unsafe_allow_html=True)                                                                                                                                         
     
 
     # --- 📊 Indicadores Económicos ---
-    st.markdown('<h3 class="fin-header">📊 Indicadores Económicos</h3>', unsafe_allow_html=True)
-    cw1, cw2, cw3 = st.columns(3)
+    st.markdown('<h3 class="app-card-titulo">📊 Indicadores Económicos</h3>', unsafe_allow_html=True)
+    cw1, cw2 = st.columns(2)
     with cw1:
-        st.markdown('<div class="app-card"><h4>xxxxx</h4><p class="tag-ia">xxxxx</p><p class="small-text-fin">xxxxxxxxxxxx.</p></div>', unsafe_allow_html=True)                                                                                                                                         
+        st.markdown('<div class="app-card"> <p class="tag-ia">xxxxx</p><p class="small-text-fin">xxxxxxxxxxxx.</p></div>', unsafe_allow_html=True)                                                                                                                                         
     
 
 
